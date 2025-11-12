f8185e337c8f53abe67e34fe9b81e41304556ba8
CT02k <joaopaulocfwork@gmail.com>
2025-11-12 17:52:33 -0300

    feat: add prettier for code formatting

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,30 +1,31 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const result = await fetch("https://ai.hackclub.com/proxy/v1/chat/completions",
+  const result = await fetch(
+    "https://ai.hackclub.com/proxy/v1/chat/completions",
     {
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.HACKCLUB_AI_API_KEY}`
+        Authorization: `Bearer ${process.env.HACKCLUB_AI_API_KEY}`,
       },
       body: JSON.stringify({
         model: "gpt-4o",
         messages: [
           {
             role: "user",
-            content: "[NÃO USE MARKDOWN] What is the capital of France?"
-          }
-        ]
+            content: "[NÃO USE MARKDOWN] What is the capital of France?",
+          },
+        ],
       }),
       method: "POST",
-    }
-  )  
+    },
+  );
 
   const data = await result.json();
 
   const response = {
-    answer: data.choices[0].message.content
-  }
+    answer: data.choices[0].message.content,
+  };
 
   return NextResponse.json(response);
 }
