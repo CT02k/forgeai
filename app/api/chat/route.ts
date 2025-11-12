1919535d1bfcd859eefa98ba8a2fea2bb3f49acc
CT02k <joaopaulocfwork@gmail.com>
2025-11-12 17:21:45 -0300

    feat(api): created base for /chat endpoint

---
+++ app/api/chat/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const result = await fetch("https://ai.hackclub.com/proxy/v1/chat/completions",
+    {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${process.env.HACKCLUB_AI_API_KEY}`
+      },
+      body: JSON.stringify({
+        model: "gpt-4o",
+        messages: [
+          {
+            role: "user",
+            content: "[NÃO USE MARKDOWN] What is the capital of France?"
+          }
+        ]
+      }),
+      method: "POST",
+    }
+  )  
+
+  const data = await result.json();
+
+  const response = {
+    answer: data.choices[0].message.content
+  }
+
+  return NextResponse.json(response);
+}
