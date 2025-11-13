d51cf2bee17a6b5cc4f2f7ffc19b21579ac0b487
CT02k <joaopaulocfwork@gmail.com>
2025-11-13 10:13:32 -0300

    style: some visual changes

--- app/(home)/components/ChatBotCard.tsx
+++ app/(home)/components/ChatBotCard.tsx
@@ -7,17 +7,17 @@ export default function ChatBotCard({ bot }: { bot: Bot }) {
   return (
     <Link
       href={`/bots/${bot.id}`}
-      className="flex gap-4 rounded-lg m-2 w-72 md:w-96 items-center p-3 bg-zinc-900 cursor-pointer hover:opacity-80 transition"
+      className="flex gap-4 rounded-lg m-2 w-72 md:w-96 items-center p-3 bg-zinc-900 border border-zinc-800 cursor-pointer hover:opacity-80 transition"
     >
       <Image
         src={bot.avatar}
         alt={`${bot.name} Avatar`}
         height={128}
         width={128}
-        className="size-16 md:size-24 rounded-full"
+        className="size-16 rounded-full"
       />
       <div>
-        <h2 className="text-2xl text-primary font-semibold mb-2">{bot.name}</h2>
+        <h2 className="text-xl text-primary font-semibold">{bot.name}</h2>
         <p className="text-zinc-300">{bot.description}</p>
       </div>
     </Link>
---
+++ app/(home)/types.ts
@@ -0,0 +1,6 @@
+export interface Bot {
+  id: string;
+  name: string;
+  avatar: string;
+  description: string;
+}
